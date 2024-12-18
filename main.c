7031ba0f5992123b1ca5b99f4661274df1ef9eff
Joyanta Kumar <joyroy122004@gmail.com>
2024-12-18 22:27:55 +0600

    Watched full tutorial

---
+++ main.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <time.h>
+#include <stdlib.h>
+
+char board[3][3];
+const char PLAYER = 'X';
+const char COMPUTER = 'O';
+
+void resetBoard();
+void printBoard();
+int checkFreeSpace();
+
+void playerMove();
+void computerMove();
+
+char checkWinner();
+void printWinner(char);
+
+int main() {
+  char winner = ' ';
+
+  resetBoard();
+  printBoard();
+
+  while (winner == ' ' && checkFreeSpace() != 0)
+  {
+    playerMove();
+    computerMove();
+
+    printBoard();
+
+    winner = checkWinner();
+    if (winner != ' ' || checkFreeSpace() == 0) {
+      break;
+    }
+  }
+
+  printWinner(winner);
+
+  return 0;
+}
+
+void resetBoard()
+{
+  for (int i = 0; i < 3; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      board[i][j] = ' ';
+    }
+  }
+}
+
+void printBoard()
+{
+  printf(" %c | %c | %c ", board[0][0], board[0][1], board[0][2]);
+  printf("\n---|---|---\n");
+  printf(" %c | %c | %c ", board[1][0], board[1][1], board[1][2]);
+  printf("\n---|---|---\n");
+  printf(" %c | %c | %c \n", board[2][0], board[2][1], board[2][2]);
+}
+
+int checkFreeSpace()
+{
+  int freeSpaces = 9;
+  for (int i = 0; i < 3; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      if (board[i][j] != ' ')
+      {
+        freeSpaces--;
+      }
+    }
+  }
+  return freeSpaces;
+}
+
+void playerMove()
+{
+  int x, y;
+  while (1)
+  {
+    printf("Enter cell: ");
+    scanf("%d %d", &x, &y);
+    x--;
+    y--;
+
+    if (board[x][y] == ' ')
+    {
+      board[x][y] = PLAYER;
+      break;
+    }
+    else
+    {
+      printf("Invalid move!\n");
+    }
+  }
+}
+
+void computerMove()
+{
+  srand(time(0));
+  int x;
+  int y;
+  if (checkFreeSpace() > 0)
+  {
+    do 
+    {
+      x = rand() % 3;
+      y = rand() % 3;
+    } while (board[x][y] != ' ');
+
+    board[x][y] = COMPUTER;
+  }
+  else
+  {
+    printWinner(' ');
+  }
+}
+
+char checkWinner()
+{
+  // Check rows
+  for (int i = 0; i < 3; i++)
+  {
+    if (board[i][0] == board[i][1] && board[i][0] == board[i][2])
+    {
+      return board[i][0];
+    }
+  }
+  // Check columns
+  for (int i = 0; i < 3; i++)
+  {
+    if (board[0][i] == board[1][i] && board[0][i] == board[2][i])
+    {
+      return board[0][i];
+    }
+  }
+  // Check diagonals
+  if (board[0][0] == board[1][1] && board[0][0] == board[2][2])
+  {
+    return board[0][0];
+  }
+  else if (board[0][2] == board[1][1] && board[0][2] == board[2][0])
+  {
+    return board[0][0];
+  }
+  return ' ';
+}
+
+void printWinner(char winner)
+{
+  if (winner == PLAYER)
+  {
+    printf("YOU WIN.\n");
+  }
+  else if (winner == COMPUTER)
+  {
+    printf("YOU LOOSE.\n");
+  }
+  else
+  {
+    printf("DRAW.\n");
+  }
+}
